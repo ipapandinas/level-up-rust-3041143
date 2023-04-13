f7eac14953220fbcaf56050d58012ae0fee127c8
Igor Papandinas <igor.papandinas@posteo.net>
2023-04-13 06:50:15 +0200

    task: 01_05b exercice done

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,39 @@
-#[derive(Debug, PartialEq)]
-enum Pulse {
+pub trait MorseCode {
+    fn to_morse_code(&self) -> Message;
+}
+
+type Message = Vec<Letter>;
+type Letter = Vec<Pulse>;
+
+pub enum Pulse {
     Short,
     Long,
 }
 
-/// Represents a single character
-type Letter = Vec<Pulse>;
+impl std::fmt::Display for Pulse {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Pulse::Short => write!(f, "."),
+            Pulse::Long => write!(f, "_"),
+        }
+    }
+}
 
-/// Represents a string of characters
-type Message = Vec<Letter>;
-
-trait MorseCode {
-    fn to_morse_code(&self) -> Message;
+pub fn print_morse_code(code: &Message) {
+    for letter in code {
+        for pulse in letter {
+            print!("{}", pulse)
+        }
+        print!(" ");
+    }
+    println!();
 }
 
 impl MorseCode for String {
     fn to_morse_code(&self) -> Message {
         use Pulse::*;
 
-        let mut msg = Vec::with_capacity(self.len());
+        let mut message = Vec::with_capacity(self.len());
         for c in self.chars() {
             let letter = match c {
                 'A' | 'a' => vec![Short, Long],
@@ -61,62 +76,9 @@ impl MorseCode for String {
                 _ => continue,
             };
 
-            msg.push(letter);
+            message.push(letter);
         }
-        msg
+
+        message
     }
-}
-
-impl std::fmt::Display for Pulse {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Pulse::Short => write!(f, "."),
-            Pulse::Long => write!(f, "_"),
-        }
-    }
-}
-
-fn print_morse_code(code: &Message) {
-    for letter in code.iter() {
-        for pulse in letter.iter() {
-            print!("{}", pulse);
-        }
-        print!(" ");
-    }
-    println!();
-}
-
-fn main() {
-    let greeting = "Hello, world".to_string().to_morse_code();
-
-    print_morse_code(&greeting);
-}
-
-#[test]
-fn hello_world() {
-    use Pulse::*;
-
-    let expected = vec![
-        vec![Short, Short, Short, Short, Short],
-        vec![Short],
-        vec![Short, Long, Short, Short],
-        vec![Short, Long, Short, Short],
-        vec![Long, Long, Long],
-        vec![Short, Long, Long],
-        vec![Long, Long, Long],
-        vec![Short, Long, Short],
-        vec![Short, Long, Short, Short],
-        vec![Long, Short, Short],
-    ];
-
-    let actual = "Hello, world".to_string().to_morse_code();
-    assert_eq!(actual, expected);
-}
-
-#[test]
-fn whole_alphabet() {
-    let alphabet = "abcdefghijklmnopqrstuvwxyz1234567890".to_string();
-
-    alphabet.to_morse_code();
-    alphabet.to_uppercase().to_morse_code();
 }
