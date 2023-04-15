eaa1db4981353e65356709a6c0c2c04b52be44c8
Igor Papandinas <igor.papandinas@posteo.net>
2023-04-15 14:12:22 +0200

    task: 01_11b exercice done

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,34 @@ struct Isbn {
     digits: Vec<u8>,
 }
 
+#[derive(Debug)]
+enum ParseIsbnError {
+    TooShort,
+    TooLong,
+    InvalidLastDigit,
+}
+
 impl FromStr for Isbn {
-    type Err = (); // TODO: replace with appropriate type
+    type Err = ParseIsbnError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        todo!();        
+        if s.len() < 17 {
+            return Err(ParseIsbnError::TooShort);
+        } else if s.len() > 17 {
+            return Err(ParseIsbnError::TooLong);
+        }
+
+        let digits = s.replace("-", "").into_bytes();
+        let check_digit = calculate_check_digit(&digits[..digits.len() - 1]);
+
+        if digits.last().unwrap_or(&0) != &check_digit {
+            return Err(ParseIsbnError::InvalidLastDigit);
+        }
+
+        Ok(Isbn {
+            raw: String::from(s),
+            digits,
+        })
     }
 }
 
@@ -21,13 +44,24 @@ impl std::fmt::Display for Isbn {
 
 // https://en.wikipedia.org/wiki/International_Standard_Book_Number#ISBN-13_check_digit_calculation
 fn calculate_check_digit(digits: &[u8]) -> u8 {
-    todo!()
+    let mut res = 0;
+    for (idx, digit) in digits.iter().enumerate() {
+        if idx % 2 == 0 {
+            res += digit - b'0';
+        } else {
+            res += (digit - b'0') * 3;
+        }
+    }
+    (10 - res % 10) % 10
 }
 
 fn main() {
     let rust_in_action: Isbn = "978-3-16-148410-0".parse().unwrap();
-
-    println!("Rust in Action's ISBN-13 ({})is valid!", rust_in_action);
+    let test = calculate_check_digit(&rust_in_action.digits);
+    println!(
+        "Rust in Action's ISBN-13 ({}) is valid with digit calculated: {}!",
+        rust_in_action, test
+    );
 }
 
 #[test]
